d139fac4ac0ef3cae3bc57b6fba973e3b879fb9a
nuknal <linktolane@gmail.com>
2025-09-08 15:41:36 +0800

    LICENSE

--- src/scripts/seed-all-tables.ts
+++ src/scripts/seed-all-tables.ts
@@ -1,8 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Client } from 'pg';
-import {
-  users,
-} from '@/db/schema';
+import { users } from '@/db/schema';
 
 export async function connectDb() {
   const client = new Client({ connectionString: process.env.DATABASE_URL });
